c1ecbfe25a758f618567343bbb9e40f2dadbd921
Christoph Bessei <chris@bessei-it.eu>
2017-12-22 09:03:41 +0100

    Initial commit

---
+++ dhv-jugend.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: DHV-Jugend
+ * Plugin URI: https://github.com/DHV-Jugend/dhv-jugend
+ * Description: WP site package (plugin) for www.dhv-jugend.de
+ * Version: 0.1.2
+ * Text Domain: dhv-jugend
+ * Domain Path: /languages
+ * Author: Christoph Bessei
+ * Author URI: https://www.dhv-jugend.de
+ * License: GPLv2
+ */
